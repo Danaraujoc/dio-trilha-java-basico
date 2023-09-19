bdd87d0ff1161dafa2fdbdc273a910e65052fd70
Danaraujoc <daniel.araujox77@gmail.com>
2023-09-19 20:21:33 -0300

    alterações estruturas de repetição

---
+++ codigo-fonte/Conhecendo-a-Linguagem-Programação-Java/Introdução a Estruturas Condicionais com Java/controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,18 @@
+// ExemploFor.java
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        /*
+         * for (int x = 0; x < alunos.length; x++) {
+         * System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+         * }
+         */
+
+        // Exemplo For each
+        // Forma abreviada
+        for (String aluno : alunos) {
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+}
